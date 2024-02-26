b5f8ec05581013eb9832ead514e286715749df92
gauravsahoo098 <159449508+gauravsahoo098@users.noreply.github.com>
2024-02-27 01:04:57 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -17,7 +17,7 @@ class Var(object):
     WORKERS = int(getenv('WORKERS', '50'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002040564082'))
     PORT = int(getenv('PORT', '8080'))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '212.224.88.31'))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'https://qrpay.fun'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "52014953230").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
@@ -29,7 +29,7 @@ class Var(object):
     
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', '212.224.88.31')) if not ON_HEROKU or getenv('FQDN', '212.224.88.31') else APP_NAME+'.herokuapp.com'
+    FQDN = str(getenv('FQDN', '103.212.120.43')) if not ON_HEROKU or getenv('FQDN', '103.212.120.43') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
