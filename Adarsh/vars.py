b5bfb3f801bdfe7e355e1e81bc6e97c0536ffe5a
tarkeshgoswami3 <154048438+tarkeshgoswami3@users.noreply.github.com>
2024-02-28 18:28:42 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,20 +9,20 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '20108610'))
-    API_HASH = str(getenv('API_HASH', '262418f5ebe0a599821a5802a90ee1a5'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '6590978998:AAFxHZcqaRmDUeZ64mk9pCYy-n08bYVyAtI'))
-    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "sahoovpsbot")
+    API_ID = int(getenv('API_ID', '29689475'))
+    API_HASH = str(getenv('API_HASH', 'e550c215a6720df4c747ddf8febf1686'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '6993152634:AAEc-ztX6jmSeoC81lRpQXhyROJObtI9gYM'))
+    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "animefile3bot")
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '50'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002040564082'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002034021960'))
     PORT = int(getenv('PORT', '8080'))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'https://qrpay.fun'))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'https://srv22709618.ultasrv.net/'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "2014953230").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5882105086").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'gauravsahoo'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'thenameistg'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -35,6 +35,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://gauravsahoo46:badshah098@swegos.p5nm03d.mongodb.net/?retryWrites=true&w=majority&appName=swegos'))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://narcos0017:4pXHYgjaECjVLu9i@cluster0.bottupl.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', ''))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split())) 
